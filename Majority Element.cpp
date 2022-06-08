981e6299ee601558bca536105d91db0a7cfc6dcd
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-06-08 16:41:03 +0530

    Majority Element

---
+++ Majority Element.cpp
@@ -0,0 +1,79 @@
+//Majority Element
+
+
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        
+        // your code here
+        int maj_index = 0, i,count = 1; 
+    for (int i = 1; i < size; i++) 
+    { 
+        if (a[maj_index] == a[i]) 
+          count++; 
+        else
+          count--; 
+        if (count == 0) 
+        { 
+           maj_index = i; 
+           count++; 
+        } 
+   } 
+
+   int maj_count = 0;
+    for (int i = 0; i < size; i++)
+    { 
+
+        if (a[i] == a[maj_index]) 
+          maj_count++;
+    }
+
+ 
+    if (maj_count > size/2)
+    {
+        return  a[maj_index]; 
+    }
+
+    else
+    {
+        return  -1;
+    }
+  } 
+};
+
+// { Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.majorityElement(arr, n) << endl;
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
