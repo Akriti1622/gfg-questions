035c44bf947933890ff17ff09d8a9791d8fdd71a
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-06-24 11:29:28 +0530

    Insert in Middle of Linked List

---
+++ Insert in Middle of Linked List.cpp
@@ -0,0 +1,91 @@
+//Insert in Middle of Linked List 
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+// function to display the linked list
+void display(Node* head)
+{
+	while (head != NULL) {
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+Node* insertInMiddle(Node* head, int x);
+
+int main()
+{
+    int T, n, x;
+    cin>>T;
+    while(T--)
+    {
+        cin>> n >> x;
+        
+        struct Node *head = new Node(x);
+        struct Node *tail = head;
+        
+        for (int i=0; i<n-1; i++)
+        {
+            cin>>x;
+            tail->next = new Node(x);
+            tail = tail->next;
+        }
+        
+        cin>> x;
+        head = insertInMiddle(head, x);
+        display(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to insert a node in the middle of the linked list.
+Node* insertInMiddle(Node* head, int x)
+{
+	// Code here
+	int count=0;
+	for(Node *curr=head;curr!=NULL;curr=curr->next)
+	    count++;
+	int mid;
+	if(count&1)
+	mid=count/2;
+	else
+	mid=count/2-1;
+	Node *curr=head;
+	Node *temp=new Node(x);
+	int i=0;
+	while(curr!=NULL&&i<mid)
+	{
+	    curr=curr->next;
+	    i++;
+	}
+	temp->next=curr->next;
+	curr->next=temp;
+	return head;
+}
