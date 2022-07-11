686766ebef8f766f12e523aa8b540b7a78e2f096
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-07-11 14:47:24 +0530

    Intersection of two sorted Linked lists

---
+++ Intersection of two sorted Linked lists .cpp
@@ -0,0 +1,114 @@
+//Intersection of two sorted Linked lists 
+
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node *head=new Node(0), *curr=head;
+    Node *c1=head1, *c2=head2;
+       
+    while(c1!=NULL&&c2!=NULL)
+    {
+        
+        if((c1->data)<(c2->data))
+        {
+            
+            c1=c1->next;
+        }
+        else if((c1->data)>(c2->data))
+        {
+            
+            c2=c2->next;
+        }
+         else if((c1->data)==(c2->data))
+        {
+            curr->next=new Node(c1->data);
+            curr=curr->next;
+            c1=c1->next;
+            c2=c2->next;
+        }
+    }
+    Node *temp=head->next;
+    return temp;
+}
